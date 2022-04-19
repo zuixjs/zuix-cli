b878926837862878f89d12b4bfb3674aa54e598c
Gene <genemars@users.noreply.github.com>
2022-04-19 02:53:47 +0200

    Added proper error handling in `generate.js`.

--- commands/generate.js
+++ commands/generate.js
@@ -42,67 +42,73 @@ for (let i = 0; i < 5 ; i++) {
 }
 
 async function generate(...args) {
-  let template;
-  let schematic = args[0];
-  const options = args[1];
-  switch (schematic) {
-    case 'component':
-    case 'controller':
-    case 'template':
-      template = options[0];
-      const className = classNameFromHyphens(template);
-      console.log(
+  return new Promise((resolve, reject) => {
+    let template;
+    let schematic = args[0];
+    const options = args[1];
+    switch (schematic) {
+      case 'component':
+      case 'controller':
+      case 'template':
+        template = options[0];
+        const className = classNameFromHyphens(template);
+        console.log(
           chalk.cyanBright('*') + ' Generating',
           chalk.yellow.bold(schematic),
           template, '→',
           chalk.green.bold(className)
-      );
-      // check first if already exists
-      const componentId = `${schematic}s/${template}`;
-      const destinationName = path.join(sourceFolder, 'app', `${componentId}.`);
-      if (fs.existsSync(destinationName + 'js') || fs.existsSync(destinationName + 'css') || fs.existsSync(destinationName + 'html')) {
-        console.error(
-            chalk.red.bold('A file with that name already exists.')
         );
-      } else {
-        // create output folder if does not exists
-        mkdirp.sync(path.dirname(destinationName + 'js'));
-        const templateData = {
-          componentId,
-          name: className,
-          author: process.env.LOGNAME
-        };
-        const componentTemplate = './templates/component/component.';
-        let css = fs.readFileSync(componentTemplate + 'css').toString('utf8');
-        let html = fs.readFileSync(componentTemplate + 'html').toString('utf8');
-        let js = fs.readFileSync(componentTemplate + 'js').toString('utf8');
-        if (schematic === 'component' || schematic === 'template') {
-          html = nunjucks.renderString(html, templateData);
-          fs.writeFileSync(destinationName + 'html', html);
-          console.log('-', chalk.yellow('added'), destinationName + 'html');
-          css = nunjucks.renderString(css, templateData);
-          fs.writeFileSync(destinationName + 'css', css);
-          console.log('-', chalk.yellow('added'), destinationName + 'css');
+        // check first if already exists
+        const componentId = `${schematic}s/${template}`;
+        const destinationName = path.join(sourceFolder, 'app', `${componentId}.`);
+        if (fs.existsSync(destinationName + 'js') || fs.existsSync(destinationName + 'css') || fs.existsSync(destinationName + 'html')) {
+          const rejectReason = `"${componentId}" already exists.`;
+          console.error(
+            chalk.red.bold(rejectReason)
+          );
+          reject(new Error(rejectReason));
+        } else {
+          // create output folder if does not exists
+          mkdirp.sync(path.dirname(destinationName + 'js'));
+          const templateData = {
+            componentId,
+            name: className,
+            author: process.env.LOGNAME
+          };
+          const componentTemplate = './templates/component/component.';
+          let css = fs.readFileSync(componentTemplate + 'css').toString('utf8');
+          let html = fs.readFileSync(componentTemplate + 'html').toString('utf8');
+          let js = fs.readFileSync(componentTemplate + 'js').toString('utf8');
+          if (schematic === 'component' || schematic === 'template') {
+            html = nunjucks.renderString(html, templateData);
+            fs.writeFileSync(destinationName + 'html', html);
+            console.log('-', chalk.yellow('added'), destinationName + 'html');
+            css = nunjucks.renderString(css, templateData);
+            fs.writeFileSync(destinationName + 'css', css);
+            console.log('-', chalk.yellow('added'), destinationName + 'css');
+          }
+          if (schematic === 'component' || schematic === 'controller') {
+            js = nunjucks.renderString(js, templateData);
+            fs.writeFileSync(destinationName + 'js', js);
+            console.log('-', chalk.yellow('added'), destinationName + 'js');
+          }
+          console.log('\nNEW componentId:', chalk.green.bold(componentId));
+          const type = schematic === 'controller' ? 'ctrl ' : schematic === 'template' ? 'view ' : '';
+          const htmlCode = `<div ${type}z-load="${componentId}"></div>`;
+          console.log(cutHereMark);
+          console.log(highlight(htmlCode, { language: 'html' }));
+          console.log(cutHereMark + '\n');
+          return resolve({componentId, path: destinationName + '*', html: htmlCode});
         }
-        if (schematic === 'component' || schematic === 'controller') {
-          js = nunjucks.renderString(js, templateData);
-          fs.writeFileSync(destinationName + 'js', js);
-          console.log('-', chalk.yellow('added'), destinationName + 'js');
-        }
-        console.log('\nNEW componentId:', chalk.green.bold(componentId));
-        const type = schematic === 'controller' ? 'ctrl ' : schematic === 'template' ? 'view ' : '';
-        const htmlCode = `<div ${type}z-load="${componentId}"></div>`;
-        console.log(cutHereMark);
-        console.log(highlight(htmlCode, { language: 'html' }));
-        console.log(cutHereMark + '\n');
-        return Promise.resolve({componentId, path: destinationName + '*', html: htmlCode});
-      }
-      break;
-    default:
-      console.log(
-          chalk.yellow.bold('Unknown schematic name.')
-      );
-  }
+        break;
+      default:
+        const rejectReason = 'Unknown schematic name.';
+        console.log(
+          chalk.yellow.bold(rejectReason)
+        );
+        reject(new Error(rejectReason));
+    }
+  });
 }
 
 module.exports = generate;
